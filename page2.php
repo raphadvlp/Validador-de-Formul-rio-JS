0a5f21c811c81aeadb1204d322fc0d38e7dac2bf
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2022-08-12 11:37:51 -0300

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apresentação</title>
+    <link rel="stylesheet" href="page2.css">
+</head>
+<body>
+    <div class="container">
+        <div class="aluno">
+            <span class="aluno-rapha"><i>Raphael Rodrigues</i></span>
+        </div>
+        <div class="professor">
+            <span><i>Pro. Bonieky Lacerda - B7Web</i></span>
+        </div>
+    </div>
+</body>
+</html>
